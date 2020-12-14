bc4bc137daac4c5feaf63a48acaa43cc32d94aa7
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-14 20:32:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+document.getElementById("solution").addEventListener("click", calculateAreaOfTrapezoid);
+function calculateAreaOfTrapezoid() {
+  let a = document.getElementById("side_a").value; 
+  // assigning a side (a) value to length variable 
+  let b = document.getElementById("side_b").value;
+  // assigning a side (b) value to length variable 
+  let height = document.getElementById("height").value;
+  // assigning a value to height variable
+  let area = (parseInt(a) + parseInt(b))/2*height;
+  // assigning an area variable to a trapezoid area equation
+  document.getElementById("solution").innerHTML = area + " cm2 "; // show answer
+  if (a == 0) {
+    alert("Error: Dimensions can't be a '0'");
+  } else if (a <= 0)  {
+    alert("Error: Dimensions can't be a negative integer");
+  }
+  if (b == 0) {
+    alert("Error: Dimensions can't be a '0'");
+  } else if (b <= 0)  {
+    alert("Error: Dimensions can't be a negative integer");
+  }
+  if (height == 0) {
+    alert("Error: Dimensions can't be a '0'");
+  } else if (height <= 0)  {
+    alert("Error: Dimensions can't be a negative integer");
+  }
+}
