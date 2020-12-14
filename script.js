6f62540da97d6a9e0ee5e5d755c416fe3c2af37a
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-14 22:07:19 +0000

    Trailing Spaces

--- script.js
+++ script.js
@@ -3,14 +3,10 @@
 document.getElementById('solution').addEventListener('click', calculateAreaOfTrapezoid)
 
 function calculateAreaOfTrapezoid () {
-  // assigning a side (a) value to length variable 
-  const a = document.getElementById('side-a').value 
-  // assigning a side (b) value to length variable 
-  const b = document.getElementById('side-b').value  
-  // assigning a value to height variable
-  const height = document.getElementById('height').value 
-  // assigning an area variable to a trapezoid area equation
-  const area = (parseInt(a) + parseInt(b)) / 2 * height
+  const a = document.getElementById('side_a').value // assigning a side (a) value to length variable 
+  const b = document.getElementById('side_b').value // assigning a side (b) value to length variable 
+  const height = document.getElementById('height').value // assigning a value to height variable
+  const area = (parseInt(a) + parseInt(b)) / 2 * height // assigning an area variable to a trapezoid area equation
   document.getElementById('solution').innerHTML = area + ' cm2 ' // show answer
   if (a === 0) {
     alert("Error: Dimensions can't be a '0'")
